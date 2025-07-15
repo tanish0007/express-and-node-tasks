93fca38954f49c6969f80dda6c3e2d793392725c
tanish0007 <jangratanish0007@gmail.com>
2025-07-15 22:33:14 +0530

    log method and route in console

--- Express/get-post/index.js
+++ Express/get-post/index.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 3030;
 
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.url}`);
+    next();
+})
 
 app.get('/', (req,res)=> {
     res.sendFile(__dirname+`/index.html`);
